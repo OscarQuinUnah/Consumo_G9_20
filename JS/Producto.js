94ee763ab9a2790a94046107a1f17ec7d236522f
OscarQuinUnah <oscar.quin@unah.hn>
2022-08-11 11:57:54 -0600

    Segundo commit de la Entidad Producto

--- JS/Producto.js
+++ JS/Producto.js
@@ -1,5 +1,8 @@
 var UrlProductos = 'http://20.216.41.245:90/G9_20/controller/producto_negocio.php?opc=GetProductos';
 var UrlInsertSocio = 'http://20.216.41.245:90/G9_20/controller/producto_negocio.php?opc=InsertProducto';
+var UrlGetProducto = 'http://20.216.41.245:90/G9_20/controller/producto_negocio.php?opc=GetProducto';
+var UrlUpdateProducto = 'http://20.216.41.245:90/G9_20/controller/producto_negocio.php?opc=UpdateProducto';
+var UrlDeleteProducto = 'http://20.216.41.245:90/G9_20/controller/producto_negocio.php?opc=DeleteProducto';
 
 $(document).ready(function(){
     CargarProductos();
@@ -23,6 +26,12 @@ function CargarProductos(){
                 '<td>'+ MisItems[i].CantidadDeArticulo + '</td>'+
                 '<td>'+ MisItems[i].MontoTotal + '</td>'+
                 '<td>'+ MisItems[i].AplicaImpuesto + '</td>'+
+                '<td>'+
+                '<button class="btn btn-info" onclick="CargarProducto('+ MisItems[i].NumeroDePedido +')">Editar</button>'+
+                '</td>'+
+                '<td>'+
+                '<button class="btn btn-danger" onclick="EliminarProducto('+ MisItems[i].NumeroDePedido +')">Eliminar</button>'+
+                '</td>'
             '</tr>';
             $('#DataProductos').html(Valores);
             }
@@ -57,4 +66,85 @@ function AgregarProducto(){
         }
     });
     alert('Aviso');
+}
+
+function CargarProducto(idProducto){
+    var datosproducto = {
+        NumeroDePedido: idProducto
+    };
+    var datosproductojson = JSON.stringify(datosproducto);
+
+    $.ajax({
+        url: UrlGetProducto,
+        type: 'POST',
+        data: datosproductojson,
+        datatype: 'JSON',
+        contentType: 'application/json',
+        success: function(response){
+            var MisItems = response;
+            $('#NumeroDePedido').val(MisItems[0].NumeroDePedido);
+            $('#NombreArticulo').val(MisItems[0].NombreArticulo);
+            $('#PrecioUnitario').val(MisItems[0].PrecioUnitario);
+            $('#FechaDePedido').val(MisItems[0].FechaDePedido);
+            $('#CantidadDeArticulo').val(MisItems[0].CantidadDeArticulo);
+            $('#MontoTotal').val(MisItems[0].MontoTotal);
+            $('#AplicaImpuesto').val(MisItems[0].AplicaImpuesto);
+            var btnactualizar = '<input type="submit" id="btn_actualizar" onclick="ActualizarProducto(' + MisItems[0].NumeroDePedido + ')"' +
+            'value="Actualizar Producto" class="btn btn-primary"></input>';   
+            $('#btnAgregarProducto').html(btnactualizar); 
+        }
+    });
+}
+
+function ActualizarProducto(idProducto){
+    var datosproducto = {
+        NumeroDePedido: idProducto,
+        NombreArticulo: $('#NombreArticulo').val(),
+        PrecioUnitario: $('#PrecioUnitario').val(),
+        FechaDePedido: $('#FechaDePedido').val(),
+        CantidadDeArticulo: $('#CantidadDeArticulo').val(),
+        MontoTotal: $('#MontoTotal').val(),
+        AplicaImpuesto: $('#AplicaImpuesto').val()
+    };
+    var datosproductojson = JSON.stringify(datosproducto);
+
+    $.ajax({
+        url: UrlUpdateProducto,
+        type: 'PUT',
+        data: datosproductojson,
+        datatype: 'JSON',
+        contentType: 'application/json',
+        success: function(response){
+            console.log(response);
+            alert("Producto Actualizado");
+        },
+        error: function(textStatus, errorThrow){
+            alert('Error al Actualizar Producto' + textStatus + errorThrow);
+        }
+    });
+    alert('Aviso');
+}
+
+function EliminarProducto(idProducto){
+    var datosproducto = {
+        NumeroDePedido: idProducto
+    };
+    var datosproductojson = JSON.stringify(datosproducto);
+
+    $.ajax({
+        url: UrlDeleteProducto,
+        type: 'DELETE',
+        data: datosproductojson,
+        datatype: 'JSON',
+        contentType: 'application/json',
+        success: function(response){
+            console.log(response);
+            alert("Producto Eliminado");
+        },
+        error: function(textStatus, errorThrow){
+            alert('Error al eliminar el Producto');
+        }
+    });
+    alert('Aviso');
+    CargarProductos();
 }
